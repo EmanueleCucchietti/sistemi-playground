c77a1b519cd365899a59a3353c159fe510cec7a4
EmanueleCucchietti <e.cucchietti.1732@vallauri.edu>
2022-01-20 11:37:19 +0100

    Th1Torneo

---
+++ Thread/th1_Torneo/progetto del 20 01/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace progetto_del_20_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtq1 = new System.Windows.Forms.TextBox();
+            this.txtq2 = new System.Windows.Forms.TextBox();
+            this.txtq3 = new System.Windows.Forms.TextBox();
+            this.txtq4 = new System.Windows.Forms.TextBox();
+            this.txtq5 = new System.Windows.Forms.TextBox();
+            this.txtq6 = new System.Windows.Forms.TextBox();
+            this.txtq7 = new System.Windows.Forms.TextBox();
+            this.txtq8 = new System.Windows.Forms.TextBox();
+            this.txts4 = new System.Windows.Forms.TextBox();
+            this.txts3 = new System.Windows.Forms.TextBox();
+            this.txts2 = new System.Windows.Forms.TextBox();
+            this.txts1 = new System.Windows.Forms.TextBox();
+            this.txtf2 = new System.Windows.Forms.TextBox();
+            this.txtf1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtv = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAvvia = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblSQ2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblSQ1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 16);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Quarti";
+            // 
+            // txtq1
+            // 
+            this.txtq1.Location = new System.Drawing.Point(21, 49);
+            this.txtq1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq1.Name = "txtq1";
+            this.txtq1.ReadOnly = true;
+            this.txtq1.Size = new System.Drawing.Size(132, 22);
+            this.txtq1.TabIndex = 1;
+            // 
+            // txtq2
+            // 
+            this.txtq2.Location = new System.Drawing.Point(21, 79);
+            this.txtq2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq2.Name = "txtq2";
+            this.txtq2.ReadOnly = true;
+            this.txtq2.Size = new System.Drawing.Size(132, 22);
+            this.txtq2.TabIndex = 2;
+            // 
+            // txtq3
+            // 
+            this.txtq3.Location = new System.Drawing.Point(21, 133);
+            this.txtq3.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq3.Name = "txtq3";
+            this.txtq3.ReadOnly = true;
+            this.txtq3.Size = new System.Drawing.Size(132, 22);
+            this.txtq3.TabIndex = 3;
+            // 
+            // txtq4
+            // 
+            this.txtq4.Location = new System.Drawing.Point(21, 163);
+            this.txtq4.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq4.Name = "txtq4";
+            this.txtq4.ReadOnly = true;
+            this.txtq4.Size = new System.Drawing.Size(132, 22);
+            this.txtq4.TabIndex = 4;
+            // 
+            // txtq5
+            // 
+            this.txtq5.Location = new System.Drawing.Point(21, 230);
+            this.txtq5.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq5.Name = "txtq5";
+            this.txtq5.ReadOnly = true;
+            this.txtq5.Size = new System.Drawing.Size(132, 22);
+            this.txtq5.TabIndex = 5;
+            // 
+            // txtq6
+            // 
+            this.txtq6.Location = new System.Drawing.Point(21, 260);
+            this.txtq6.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq6.Name = "txtq6";
+            this.txtq6.ReadOnly = true;
+            this.txtq6.Size = new System.Drawing.Size(132, 22);
+            this.txtq6.TabIndex = 6;
+            // 
+            // txtq7
+            // 
+            this.txtq7.Location = new System.Drawing.Point(21, 314);
+            this.txtq7.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq7.Name = "txtq7";
+            this.txtq7.ReadOnly = true;
+            this.txtq7.Size = new System.Drawing.Size(132, 22);
+            this.txtq7.TabIndex = 7;
+            // 
+            // txtq8
+            // 
+            this.txtq8.Location = new System.Drawing.Point(21, 344);
+            this.txtq8.Margin = new System.Windows.Forms.Padding(4);
+            this.txtq8.Name = "txtq8";
+            this.txtq8.ReadOnly = true;
+            this.txtq8.Size = new System.Drawing.Size(132, 22);
+            this.txtq8.TabIndex = 8;
+            // 
+            // txts4
+            // 
+            this.txts4.Location = new System.Drawing.Point(250, 274);
+            this.txts4.Margin = new System.Windows.Forms.Padding(4);
+            this.txts4.Name = "txts4";
+            this.txts4.ReadOnly = true;
+            this.txts4.Size = new System.Drawing.Size(132, 22);
+            this.txts4.TabIndex = 12;
+            // 
+            // txts3
+            // 
+            this.txts3.Location = new System.Drawing.Point(250, 244);
+            this.txts3.Margin = new System.Windows.Forms.Padding(4);
+            this.txts3.Name = "txts3";
+            this.txts3.ReadOnly = true;
+            this.txts3.Size = new System.Drawing.Size(132, 22);
+            this.txts3.TabIndex = 11;
+            // 
+            // txts2
+            // 
+            this.txts2.Location = new System.Drawing.Point(250, 153);
+            this.txts2.Margin = new System.Windows.Forms.Padding(4);
+            this.txts2.Name = "txts2";
+            this.txts2.ReadOnly = true;
+            this.txts2.Size = new System.Drawing.Size(132, 22);
+            this.txts2.TabIndex = 10;
+            // 
+            // txts1
+            // 
+            this.txts1.Location = new System.Drawing.Point(250, 123);
+            this.txts1.Margin = new System.Windows.Forms.Padding(4);
+            this.txts1.Name = "txts1";
+            this.txts1.ReadOnly = true;
+            this.txts1.Size = new System.Drawing.Size(132, 22);
+            this.txts1.TabIndex = 9;
+            // 
+            // txtf2
+            // 
+            this.txtf2.Location = new System.Drawing.Point(457, 213);
+            this.txtf2.Margin = new System.Windows.Forms.Padding(4);
+            this.txtf2.Name = "txtf2";
+            this.txtf2.ReadOnly = true;
+            this.txtf2.Size = new System.Drawing.Size(132, 22);
+            this.txtf2.TabIndex = 14;
+            // 
+            // txtf1
+            // 
+            this.txtf1.Location = new System.Drawing.Point(457, 183);
+            this.txtf1.Margin = new System.Windows.Forms.Padding(4);
+            this.txtf1.Name = "txtf1";
+            this.txtf1.ReadOnly = true;
+            this.txtf1.Size = new System.Drawing.Size(132, 22);
+            this.txtf1.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(280, 36);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Semi-Finali";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(502, 37);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Finale";
+            // 
+            // txtv
+            // 
+            this.txtv.Location = new System.Drawing.Point(662, 193);
+            this.txtv.Margin = new System.Windows.Forms.Padding(4);
+            this.txtv.Name = "txtv";
+            this.txtv.ReadOnly = true;
+            this.txtv.Size = new System.Drawing.Size(132, 22);
+            this.txtv.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(696, 37);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 16);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Vincitore";
+            // 
+            // btnAvvia
+            // 
+            this.btnAvvia.Location = new System.Drawing.Point(21, 444);
+            this.btnAvvia.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAvvia.Name = "btnAvvia";
+            this.btnAvvia.Size = new System.Drawing.Size(164, 38);
+            this.btnAvvia.TabIndex = 19;
+            this.btnAvvia.Text = "AVVIA TORNEO";
+            this.btnAvvia.UseVisualStyleBackColor = true;
+            this.btnAvvia.Click += new System.EventHandler(this.btnAvvia_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblSQ2);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblSQ1);
+            this.groupBox1.Location = new System.Drawing.Point(250, 401);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(267, 123);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // lblSQ2
+            // 
+            this.lblSQ2.AutoSize = true;
+            this.lblSQ2.Location = new System.Drawing.Point(170, 66);
+            this.lblSQ2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSQ2.Name = "lblSQ2";
+            this.lblSQ2.Size = new System.Drawing.Size(89, 16);
+            this.lblSQ2.TabIndex = 2;
+            this.lblSQ2.Text = "[SQUADRA2]";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(125, 66);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(21, 16);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "vs";
+            // 
+            // lblSQ1
+            // 
+            this.lblSQ1.AutoSize = true;
+            this.lblSQ1.Location = new System.Drawing.Point(9, 66);
+            this.lblSQ1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSQ1.Name = "lblSQ1";
+            this.lblSQ1.Size = new System.Drawing.Size(89, 16);
+            this.lblSQ1.TabIndex = 0;
+            this.lblSQ1.Text = "[SQUADRA1]";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(576, 372);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(218, 152);
+            this.textBox1.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(847, 554);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAvvia);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtv);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtf2);
+            this.Controls.Add(this.txtf1);
+            this.Controls.Add(this.txts4);
+            this.Controls.Add(this.txts3);
+            this.Controls.Add(this.txts2);
+            this.Controls.Add(this.txts1);
+            this.Controls.Add(this.txtq8);
+            this.Controls.Add(this.txtq7);
+            this.Controls.Add(this.txtq6);
+            this.Controls.Add(this.txtq5);
+            this.Controls.Add(this.txtq4);
+            this.Controls.Add(this.txtq3);
+            this.Controls.Add(this.txtq2);
+            this.Controls.Add(this.txtq1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Torneo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtq1;
+        private System.Windows.Forms.TextBox txtq2;
+        private System.Windows.Forms.TextBox txtq3;
+        private System.Windows.Forms.TextBox txtq4;
+        private System.Windows.Forms.TextBox txtq5;
+        private System.Windows.Forms.TextBox txtq6;
+        private System.Windows.Forms.TextBox txtq7;
+        private System.Windows.Forms.TextBox txtq8;
+        private System.Windows.Forms.TextBox txts4;
+        private System.Windows.Forms.TextBox txts3;
+        private System.Windows.Forms.TextBox txts2;
+        private System.Windows.Forms.TextBox txts1;
+        private System.Windows.Forms.TextBox txtf2;
+        private System.Windows.Forms.TextBox txtf1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtv;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAvvia;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblSQ2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblSQ1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
